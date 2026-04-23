7509187cfad2ce8fae63cdd6886037298e025d82
M4DRUGADOR <bac0nsolido@gmail.com>
2026-04-23 12:39:40 -0500

    [nivelacion-n1] bloque1 - tipos de datos personaje rpg

---
+++ java/com/mycompany/rpgmanager/RPGManager.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rpgmanager;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class RPGManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
